db0f3aacdf3cd91c4aec03807ae72d8643964c9d
Sasha <sashamangir@mail.ru>
2018-05-08 23:26:57 +0400

    made a beautiful code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { AppRegistry } from "react-native";
 
 import { Provider } from "react-redux";
-import configureStore from "./configureStore";
-import App from "./app";
+import configureStore from "./src/configureStore";
+import App from "./src/App";
 
 const store = configureStore();
 
